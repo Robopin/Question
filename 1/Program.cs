74eaf5fe2f3a614be81da14c80e7faff475f16a5
Robopin <vlasizov@yandex.ru>
2022-12-11 21:33:44 +0300

    Add 1

---
+++ 1/Program.cs
@@ -0,0 +1,29 @@
+﻿int GetMassive(int length)
+{
+int[] array = new int[length];
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine($"Введите {i} число массива");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+return array;
+}
+
+
+
+// int[] array = new int[5];
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine($"Введите {i} число массива");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+//int[] arr = GetMassive(5);
+PrintArray(array);
